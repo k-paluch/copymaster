e182d42b332806f4b82bac17109ef1401aeae122
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2019-01-02 12:16:47 +0100

    udpate functions

--- copymaster.c
+++ copymaster.c
@@ -13,142 +13,7 @@
 #include <math.h>
 #include "options.h"
 
-void parse_opt_to_change(struct CopymasterOptions cpm_options, int give_options[])
-{
-    give_options[0] = cpm_options.fast ? 1 : 0;
-    give_options[1] = cpm_options.slow ? 1 : 0;
-    give_options[2] = cpm_options.create ? 1 : 0;
-    give_options[3] = cpm_options.overwrite ? 1 : 0;
-    give_options[4] = cpm_options.append ? 1 : 0;
-    give_options[5] = cpm_options.lseek ? 1 : 0;
-    give_options[6] = cpm_options.directory ? 1 : 0;
-    give_options[7] = cpm_options.delete_opt ? 1 : 0;
-    give_options[8] = cpm_options.chmod ? 1 : 0;
-    give_options[9] = cpm_options.inode ? 1 : 0;
-    give_options[10] = cpm_options.umask ? 1 : 0;
-    give_options[11] = cpm_options.link ? 1 : 0;
-    give_options[12] = cpm_options.truncate ? 1 : 0;
-    give_options[13] = cpm_options.sparse ? 1 : 0;
-}
 
-void ban_one_option(int number, int runable_options[])
-{
-int ban_fast[]       = {1, 6, 11};
-int ban_slow[]       = {0, 6, 11};
-int ban_create[]     = {3, 11};
-int ban_overwrite[]  = {5, 8, 10, 11};
-int ban_append[]     = {2, 3, 5, 10, 11};
-int ban_lseek[]      = {6, 8, 10, 11, 13};
-int ban_directory[]  = {0, 1, 5, 7, 11};
-int ban_delete_opt[] = {6, 11, 12};
-int ban_chmod[]      = {11};
-int ban_inode[]      = {11};
-int ban_umask[]      = {3, 4, 5, 11};
-int ban_link[]       = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
-int ban_truncate[]   = {6, 7};
-int ban_sparse[]     = {5, 6, 11};
-    switch (number)
-    {
-        case 0:
-            for (long unsigned int i = 0; i < sizeof(ban_fast)/sizeof(int); i++) {
-                runable_options[ban_fast[i]] = 0;
-            }
-            break;
-        case 1:
-            for (long unsigned int i = 0; i < sizeof(ban_slow)/sizeof(int); i++) {
-                runable_options[ban_slow[i]] = 0;
-            }
-            break;
-        case 2:
-            for (long unsigned int i = 0; i < sizeof(ban_create)/sizeof(int); i++) {
-                runable_options[ban_create[i]] = 0;
-            }
-            break;
-        case 3:
-            for (long unsigned int i = 0; i < sizeof(ban_overwrite)/sizeof(int); i++) {
-                runable_options[ban_overwrite[i]] = 0;
-            }
-            break;
-        case 4:
-            for (long unsigned int i = 0; i < sizeof(ban_append)/sizeof(int); i++) {
-                runable_options[ban_append[i]] = 0;
-            }
-            break;
-        case 5:
-            for (long unsigned int i = 0; i < sizeof(ban_lseek)/sizeof(int); i++) {
-                runable_options[ban_lseek[i]] = 0;
-            }
-            break;
-        case 6:
-            for (long unsigned int i = 0; i < sizeof(ban_directory)/sizeof(int); i++) {
-                runable_options[ban_directory[i]] = 0;
-            }
-            break;
-        case 7:
-            for (long unsigned int i = 0; i < sizeof(ban_delete_opt)/sizeof(int); i++) {
-                runable_options[ban_delete_opt[i]] = 0;
-            }
-            break;
-        case 8:
-            for (long unsigned int i = 0; i < sizeof(ban_chmod)/sizeof(int); i++) {
-                runable_options[ban_chmod[i]] = 0;
-            }
-            break;
-        case 9:
-            for (long unsigned int i = 0; i < sizeof(ban_inode)/sizeof(int); i++) {
-                runable_options[ban_inode[i]] = 0;
-            }
-            break;
-        case 10:
-            for (long unsigned int i = 0; i < sizeof(ban_umask)/sizeof(int); i++) {
-                runable_options[ban_umask[i]] = 0;
-            }
-            break;
-        case 11:
-            for (long unsigned int i = 0; i < sizeof(ban_link)/sizeof(int); i++) {
-                runable_options[ban_link[i]] = 0;
-            }
-            break;
-        case 12:
-            for (long unsigned int i = 0; i < sizeof(ban_truncate)/sizeof(int); i++) {
-                runable_options[ban_truncate[i]] = 0;
-            }
-            break;
-        case 13:
-            for (long unsigned int i = 0; i < sizeof(ban_sparse)/sizeof(int); i++) {
-                runable_options[ban_sparse[i]] = 0;
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-void ban_options(int options_amount, int opt_to_change[], int runable_options[])
-{
-    for (int i = 0; i < options_amount; i++)
-    {
-        if (opt_to_change[i] == 1)
-        {
-            ban_one_option(i, runable_options);
-        }
-    }
-}
-
-void check_options(int options_amount, int opt_to_change[], int runable_options[])
-{
-    for (int i = 0; i < options_amount; i++)
-    {
-        if (opt_to_change[i] == 1)
-        {
-            if (runable_options[i] == 0)
-            {
-                fprintf(stderr, "CHYBA PREPINACOV\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-}
 
 int is_regular_file(const char *path)
 {
@@ -171,18 +36,50 @@ void FatalError(char c, const char* msg, int exit_status)
 int main(int argc, char* argv[])
 {
     struct CopymasterOptions cpm_options = ParseCopymasterOptions(argc, argv);
-    int options_amount = 14;
-
-    int runable_options[options_amount];
-    int opt_to_change[options_amount];
-
-	for (int i=0; i < options_amount; i++)
-    {
-        runable_options[i] = 1;
+	if(cpm_options.directory && (cpm_options.lseek + cpm_options.delete_opt + cpm_options.link + cpm_options.truncate)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	if(cpm_options.link && (cpm_options.append + cpm_options.create + cpm_options.directory + cpm_options.sparse + cpm_options.lseek + cpm_options.overwrite)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	if(cpm_options.sparse && cpm_options.link){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	if(cpm_options.overwrite && (cpm_options.append + cpm_options.create + cpm_options.link)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	if (cpm_options.fast && cpm_options.slow) {
+        fprintf(stderr, "CHYBA PREPINACOV\n"); 
+        exit(EXIT_FAILURE);
     }
-    parse_opt_to_change(cpm_options, opt_to_change);
-    check_options(options_amount, opt_to_change, runable_options);
-    
+	if(cpm_options.create && cpm_options.delete_opt){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	if(cpm_options.append && (cpm_options.create + cpm_options.overwrite + cpm_options.link)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	 if(cpm_options.lseek && (cpm_options.append + cpm_options.directory + cpm_options.link)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);	
+	}
+	if(cpm_options.create && (cpm_options.append + cpm_options.overwrite + cpm_options.link)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}	
+	if(cpm_options.truncate && (cpm_options.directory + cpm_options.delete_opt)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
+	if(cpm_options.delete_opt && (cpm_options.directory + cpm_options.truncate)){
+		fprintf(stderr, "CHYBA PREPINACOV\n");
+		exit(EXIT_FAILURE);
+	}
     if(argc == 3){
 	long sizeofbuffer;
     	int infile, outfile, temp;
@@ -190,7 +87,7 @@ int main(int argc, char* argv[])
         	FatalError('?', "infile", 21);
     	}
 	if( (outfile = open(cpm_options.outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644) ) == -1){ 
-        	FatalError('?', "infile", 21);
+        	FatalError('?', "outfile", 21);
     	}
     	sizeofbuffer = lseek(infile, 0, SEEK_END);
     	char buffer[sizeofbuffer];
@@ -202,24 +99,7 @@ int main(int argc, char* argv[])
     	close(outfile);
     }
 
-    if(cpm_options.fast){
-	long sizeofbuffer;
-    	int infile, outfile, temp;
-	if((infile = open(cpm_options.infile, O_RDONLY)) == -1){
-        	FatalError('f', "infile", 21);
-    	}
-	if( (outfile = open(cpm_options.outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644) ) == -1){ 
-        	FatalError('f', "infile", 21);
-    	}
-    	sizeofbuffer = lseek(infile, 0, SEEK_END);
-    	char buffer[sizeofbuffer];
-    	lseek(infile, 0, SEEK_SET);
-    	if((temp = read(infile, &buffer, sizeofbuffer)) > 0){
-        	write(outfile, &buffer, temp);
-    	}
-    	close(infile);
-    	close(outfile);
-    }
+    
 
     if(cpm_options.slow)
     {
@@ -229,13 +109,44 @@ int main(int argc, char* argv[])
         	FatalError('s', "infile", 21);
     	}
 	if( (outfile = open(cpm_options.outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644) ) == -1){ 
-        	FatalError('s', "infile", 21);
+        	FatalError('s', "outfile", 21);
     	}
     	while((temp = read(infile, &buffer, 1)) > 0){
         	write(outfile, &buffer, temp);
     	}
     	close(infile);
     	close(outfile);
+    }
+	if(cpm_options.fast){
+	long sizeofbuffer;
+    	int infile, outfile, temp;
+	if((infile = open(cpm_options.infile, O_RDONLY)) == -1){
+        	FatalError('f', "infile", 21);
+    	}
+	if( (outfile = open(cpm_options.outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644) ) == -1){ 
+        	FatalError('f', "outfile", 21);
+    	}
+    	sizeofbuffer = lseek(infile, 0, SEEK_END);
+    	char buffer[sizeofbuffer];
+    	lseek(infile, 0, SEEK_SET);
+    	if((temp = read(infile, &buffer, sizeofbuffer)) > 0){
+        	write(outfile, &buffer, temp);
+    	}
+    	close(infile);
+    	close(outfile);
+    }
+
+    if(cpm_options.link){
+	    int infile,outfile;
+	    if((infile = open(cpm_options.infile,O_RDONLY)) ==-1){
+		FatalError('K',"infile",30);
+	    }
+	    if((outfile = open(cpm_options.outfile,O_RDONLY)) == -1){
+		FatalError('K',"outfile",30);
+	    }
+	    link(cpm_options.infile,cpm_options.outfile);
+	    close(infile);
+	    close(outfile);
     }
 if(cpm_options.lseek){
         int infile, outfile;
@@ -244,16 +155,13 @@ if(cpm_options.lseek){
         	FatalError('l', "infile", 21);
     	}
         if ( (outfile = open(cpm_options.outfile, O_WRONLY) ) ==-1 ){
-            printf("Error:\no:%d\no:%s\no:%s\n", errno, strerror(errno), "SUBOR NEEXISTUJE");
-            exit(24);
+			FatalError('l',"outfile",24);
         }
         if(lseek(infile, cpm_options.lseek_options.pos1, SEEK_SET) < 0){
-            printf("Error:\nl:%d\nl:%s\nl:%s\n", errno, strerror(errno), "CHYBA POZICIE infile");
-            return 33;
+			FatalError('l',"infile",33);
         }
         if(lseek(outfile, cpm_options.lseek_options.pos2, cpm_options.lseek_options.x) < 0){
-            printf("Error:\nl:%d\nl:%s\nl:%s\n", errno, strerror(errno), "CHYBA POZICIE outfile");
-            return 33;
+			FatalError('l',"outfile",33);
         }
         char buffer[ (int) cpm_options.lseek_options.num];
         int read_bytes, written_bytes;
@@ -276,8 +184,7 @@ if(cpm_options.lseek){
     	}
         umask(0);
         if ((outfile = open(cpm_options.outfile, O_WRONLY | O_EXCL | O_CREAT, cpm_options.create_mode)) == -1){
-            printf("Error:\nc:%d\nc:%s\nc:%s\n", errno, strerror(errno), "SUBOR EXISTUJE");
-            exit(23);
+			FatalError('c',"infile",23);
         }
         while((temp = read(infile, &buffer, 1)) > 0)
         {
@@ -347,8 +254,7 @@ if (cpm_options.truncate) {
     		}
         	if ((outfile = open(cpm_options.outfile, O_WRONLY|O_TRUNC))==-1)
         	{
-            		printf("Error:\no:%d\no:%s\no:%s\n", errno, strerror(errno), "SUBOR NEEXISTUJE");
-            		exit(24);
+				FatalError('o',"outfile",24);
         	}
         	while( (temp = read(infile, &buffer, 1)) > 0 )
         	{	
@@ -422,13 +328,13 @@ if (cpm_options.truncate) {
 		}
 		if (S_ISDIR(stat_struct_check_dir.st_mode)){
 		    file_type = "directory";
-		    printf("It is a directory, the file_type temp is %s\n", file_type);
+		    printf("Je to Dir, the file_type temp is %s\n", file_type);
 		} else {
-		    printf("It is not a directory");
+		    printf("Nieje to Dir");
 		}
 		dir_var = opendir(cpm_options.infile);
 		if(dir_var){
-		    printf("Directory exists and it is opened\n");
+		    printf("Dir existuje a je otvoreny\n");
 		    while( ( dir_struct = readdir(dir_var) ) != NULL){
 		        if(strcmp(dir_struct->d_name, ".")!=0 && strcmp(dir_struct->d_name, "..")!=0){
 		            char filePath[100];
